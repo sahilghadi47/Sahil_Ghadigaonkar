b13710637d21623487093ec81e7b07ab19d80ed1
sahilghadi47 <ghadigaonkarsahil0407@gmail.com>
2025-01-28 12:51:11 +0530

    add @vercel/analytics dependency and integrate analytics component in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from "@vercel/analytics/react"
 import './index.css'
 import Router from "./Router"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Analytics />
     <Router />
   </StrictMode>,
 )
